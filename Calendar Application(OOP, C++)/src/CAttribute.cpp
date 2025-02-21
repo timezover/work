3661a800fb4ebcfa0dbe322746be5dab86fe7537
timezover <167890261+timezover@users.noreply.github.com>
2025-02-21 17:18:11 +0100

    Delete Calendar Application(OOP, C++) directory

--- Calendar Application(OOP, C++)/src/CAttribute.cpp
+++
@@ -1,9 +0,0 @@
-#include "CAttribute.h"
-
-bool CAttribute::empty() const {
-    return true;
-}
-
-void CAttribute::setAttr(const string &newAttr) {}
-
-void CAttribute::inputAttr(const string & str) {}
--- Calendar Application(OOP, C++)/src/CCalendar.cpp
+++
@@ -1,3 +0,0 @@
-#include "CCalendar.h"
-
-void CCalendar::printCalendar(const string & date, const map<string, CEvent> &m) const {}
